f2c21fadb6b95041e13d765ea911953fcc1a9d9b
andreapanico12 <andreapanico12@gmail.com>
2024-11-19 16:44:12 +0100

    feat: add exercise instructions to pari-dispari-script

---
+++ pari e dispari/pari-dispari-script.js
@@ -0,0 +1,18 @@
+// CONSEGNA:
+/* 
+Pari e Dispari:
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.   */
+
+// RAGIONAMENTO
+
+/*
+1) Definisco le variabili necessarie, in questo caso dovranno essere diverse: il numero che inserisce il giocatore e la sua giocata di pari o dispari, il numero del computer che sarà dato da una funzione, la somma dei due numeri ed infine il messaggio che sarà la dichiarazione di vittoria.
+2) La prima funzione sarà la generazione del numero mentre la seconda servirà per verificare se la variabile somma è pari o dispari (if)
+3) Alla fine ci sarà una condzione (if) che verifica se l'outcome della seconda funzione coincide con la giocata iniziale pari o dispari 
+4) Dovrà esserci quindi un messaggio finale che dichiara la vittoria in base all'ultimo if*/
+
+
