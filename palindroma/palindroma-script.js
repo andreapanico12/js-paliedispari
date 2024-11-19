662827b7a5e596de71d69376f835c6425d4193e3
andreapanico12 <andreapanico12@gmail.com>
2024-11-19 15:27:17 +0100

    feat: add exercise instruction to palindroma-script

---
+++ palindroma/palindroma-script.js
@@ -0,0 +1,14 @@
+// CONSEGNA:
+/*
+Palidroma:
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma */
+
+//RAGIONAMENTO
+
+/* 1) definisco la variabile prompt che servirà ad inizializzare il tutto.
+  2)La funzione dovrà contenere un modo di invertire l'ordine delle lettere di una parola salvandola come variabile a se stante.
+  
+  3)La stessa funzione dovrà confrontare se la variabile iniziale e quella invertita dovessero essere uguali
+  
+  4)La funzione infine restituirà un messaggio in base a che le variabili risultino uguali oppure no*/
